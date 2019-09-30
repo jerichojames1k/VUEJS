a5f52c775fad6846912b354bf233ba429b31dfa3
jerichojames1k <jerichojames1k>
2019-09-30 23:57:36 +0800

    the FINAL VUEJS

---
+++ tutorial/src/components/frame/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="Header">
+    <b-navbar  toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#" id="title">            <b-card-img :src="require('assets/home.png')" class="rounded-0" id="header"></b-card-img></b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item v-on:click="redirect('/register')">Register</b-nav-item>
+          <b-nav-item v-on:click="redirect('/login')">Login</b-nav-item> 
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "~assets/colors.scss";
+#title {
+  color: $warning !important;
+}
+</style>
+
+<script>
+import ROUTER from "router";
+export default {
+  methods: {
+    redirect(route) {
+      ROUTER.push(route);
+    }
+  }
+};
+</script>
+
+<style scoped>
+#home{
+	width:5%; 
+	height: auto;
+}
+#header{
+  width:30%;
+  height:auto;
+}
+</style>
---
+++ tutorial/src/services/auth/index.js
@@ -0,0 +1,55 @@
+import ROUTER from 'router';
+
+export default {
+    user: null,
+    registeredUser: [],
+    courses:[],
+    setUser(user){
+        this.user = user
+    },
+    getUser(user){
+        return this.user = user;
+    },
+    register(username, password){
+        this.registeredUser.push({
+            username: username,
+            password: password,
+        })
+        // var p = JSON.parse(JSON.stringify(this.registeredUser))
+        // console.log(p)
+        ROUTER.push('/login')
+    },
+    login(username,password){
+        for(let i = 0; i < this.registeredUser.length; i++){
+            if(this.registeredUser[i].username === username && this.registeredUser[i].password === password){
+                ROUTER.push('/dashboard')
+                return this.registeredUser[i]           
+            }else{
+                alert("Email or Password is incorrect!")
+            }
+        }
+        return null
+    },
+    logout(){
+        this.user = null
+        ROUTER.push('/login')
+    },
+    addCourse(course, year){
+        this.courses.push({
+            course: course,
+            year: year,
+        });
+        var p = JSON.parse(JSON.stringify(this.courses))
+        console.log(p)
+    },
+    // getCourse(){
+    //     for(let i = 0; i<this.courses.length-1; i++){
+             
+    //         return this.courses[i]
+    //     }
+       
+    // }  
+    editProfile(){
+        ROUTER.push('/edit')
+    }
+}
