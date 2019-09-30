be4d11b280f9c1f218fdef5a848494e715cfdff9
jerichojames1k <jerichojames1k>
2019-09-30 22:11:40 +0800

    first commit

---
+++ src/components/frame/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="Header">
+    <b-navbar  toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#" id="title">Welcome to MyApplication</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item v-on:click="redirect('/register')">Register</b-nav-item>
+          <b-nav-item v-on:click="redirect('/login')">Login</b-nav-item> 
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "~assets/colors.scss";
+#title {
+  color: $warning !important;
+}
+</style>
+
+<script>
+import ROUTER from "router";
+export default {
+  methods: {
+    redirect(route) {
+      ROUTER.push(route);
+    }
+  }
+};
+</script>
+
+<style scoped>
+#home{
+	width:30%; 
+	height: auto;
+}
+</style>
---
+++ src/components/frame/sidebar.vue
@@ -0,0 +1,56 @@
+<template>
+  <sidebar-menu :menu="menu" />
+</template>
+ 
+ <script>
+ import AUTH from 'services/auth'
+    export default {
+        data() {
+            return {
+                auth: AUTH,
+                menu: [
+                    {
+                        header: true,
+                        title: 'Main Navigation',
+                        hiddenOnCollapse: true
+                    },
+                    {
+                        href: '/dashboard',
+                        title: 'Dashboard',
+                        
+                        icon: 'fa fa-user'
+                    },
+                    {
+                        href: '/personalinformation',
+                        title: 'Personal Information',
+                        icon: 'fa fa-user'
+                    },
+                    
+                    {
+                        href: '/courses',
+                        title: 'Add Course & Subjects',
+                        icon: 'fa fa-book',
+                        // child: [
+                        //     {
+                        //         href: '/charts/sublink',
+                        //         title: 'Edit'
+                        //     }
+                        // ]
+                    },
+                    {
+                        href: '/login',
+                        title: 'Log Out',
+                        icon: 'fa fa-user',
+                        // AUTH.logout()
+                    
+                    }
+                ]
+            }
+        },
+        methods:{
+            // onToggleCollapse(collapsed) {},
+            // onItemClick(event, item) {}
+        }
+    }
+</script> 
+   
